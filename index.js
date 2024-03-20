b9df4ead38bb2ae23843dd352432dd4e4e49b319
dorcas Chepkirui <dorcaschepkirui22@gmail.com>
2024-03-20 12:01:58 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation){
+    const hqLocation = 42; 
+    if (distanceFromHqInBlocks()){
+        distance=  hqLocation - distanceFromHqInBlocks(43);
+}
+else if (distanceFromHqInBlocks()){
+    distance = hqLocation - distanceFromHqInBlocks(50);
+}
+else if (distanceFromHqInBlocks()){
+    distance = hqLocation - distanceFromHqInBlocks(34);
+}
+return distance;
+}
+
+function distanceFromHqInFeet(){
+    const feetPerBlock = 264;
+    let distanceInFeet = distanceFromHqInBlocks() * feetPerBlock;
+    return distanceInFeet;
+}
+/*function distanceTravelledInFeet (pickupLocation, destinationBlock){
+    const feetPerBlock = 264;
+    return Math.abs(distanceFromHqInBlocks(34) - feetPerBlock(43,44)
+}
+function calculateFarePrice(start, destination){
+
+}*/
